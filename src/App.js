bfc96ab80f6cce087fcb871ab016e9df57c1a2b9
Chandler Roth <hey@chandlerroth.com>
2022-04-04 10:53:15 -0400

    Added multi-menu item display
    Co-authored-by: Jeremy Hunton <HunJer93@users.noreply.github.com>
    Co-authored-by: N Adkin <NAdkin@users.noreply.github.com>
    Co-authored-by: Matthew Thornton <ThorntonMatthewD@users.noreply.github.com>
    Co-authored-by: ggroshansii <ggroshansii@users.noreply.github.com>
    Co-authored-by: mtigzoe <mtigzoe@users.noreply.github.com>
    Co-authored-by: Jeffrey Willis <jeffreywillis@my.tridenttech.edu>
    Co-authored-by: laurajwarren44@gmail.com <laurajwarren44@gmail.com>
    Co-authored-by: Ariana <arianakonhilas@users.noreply.github.com>
    Co-authored-by: Eric Moore <emoore501@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({ items }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {items.map(fred => (
+        <div className="menu-item">
+          <h1>{fred.title}</h1>
+          <p>{fred.price}</p>
+          <p>{fred.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { data as MENU_ITEMS } from '../fixtures/menuItems.json'
+
+it('should show the title, price, and description of a menu item when rendered', () => {
+  // Arrange
+  const expectedMenuItems = [MENU_ITEMS[0]]
+  
+  render(<App items={expectedMenuItems}/>)
+
+  // Act
+
+  // Assert
+  expect(screen.getByText(expectedMenuItems[0].title)).not.toBeNull()
+  expect(screen.getByText(expectedMenuItems[0].price)).not.toBeNull()
+  expect(screen.getByText(expectedMenuItems[0].description)).not.toBeNull()
+})
+
+it('should display multiple menu items when multiple menu items are returned from the API', () => {
+  // Arrange
+  const expectedMenuItems = MENU_ITEMS
+  render(<App items={expectedMenuItems}/>)
+
+  // Act
+
+  // Assert
+  expectedMenuItems.forEach(item => {
+    expect(screen.getByText(item.title)).not.toBeNull()
+    expect(screen.getByText(item.price)).not.toBeNull()
+    expect(screen.getByText(item.description)).not.toBeNull()
+  })
+})
 
 test('renders learn react link', () => {
   render(<App />);
